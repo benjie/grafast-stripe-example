428870a84e620147feaaa5568b297d4ea114540d
Benjie Gillam <benjie@jemjie.com>
2023-09-08 15:20:59 +0100

    Export plan diagram

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
+import { writeFile } from "node:fs/promises";
 import { grafast } from "grafast";
+import { planToMermaid } from "grafast/mermaid";
+import "graphile-config";
+import { ExecutionResult } from "graphql";
 import { schema } from "./schema.js";
 import { waitForDocker } from "./utils.js";
 
 async function main() {
   await waitForDocker();
 
-  const result = await grafast({
+  const result = (await grafast({
     schema,
     source: /* GraphQL */ `
       {
@@ -30,9 +34,28 @@ async function main() {
         stripeCustomerId: "cus_9s6XKzkNRiz8i3",
       },
     },
-  });
+    resolvedPreset: {
+      grafast: {
+        explain: true,
+      },
+    },
+  })) as ExecutionResult;
 
-  console.dir(result, { depth: 100 });
+  if (result.errors) {
+    console.dir(result.errors);
+    throw new Error(`GraphQL request raised an error`);
+  }
+
+  console.log("Query result:");
+  console.dir(result.data, { depth: 100 });
+
+  await writeFile(
+    `plan.mermaid`,
+    planToMermaid((result.extensions?.explain as any)?.operations?.[0]?.plan, {
+      skipBuckets: true,
+      concise: true,
+    }),
+  );
 }
 
 main().catch((e) => {
